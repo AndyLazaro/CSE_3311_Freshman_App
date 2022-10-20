a1ea31576279f6439b4c108a02164ec1b0e2c2ea
ElementalWisp <creeperjack75@gmail.com>
2022-10-20 10:27:26 -0500

    small updates to create club xml

---
+++ app/src/main/java/com/example/cse_3311_freshman_app/CreateClubActivity.java
@@ -0,0 +1,14 @@
+package com.example.cse_3311_freshman_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class CreateClubActivity
+{
+    Spinner categorySpinner = findViewById(R.id.spinner_category);
+}
