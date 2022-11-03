96652b2ad91ea10f23948e3fa153dc2e16281344
ChonrevC <chonrevcal@att.net>
2022-11-03 16:24:36 -0500

    Initialized placeholders and declared them to IDs

--- app/src/main/java/com/example/cse_3311_freshman_app/ClubProfileActivity.java
+++ app/src/main/java/com/example/cse_3311_freshman_app/ClubProfileActivity.java
@@ -1,32 +1,65 @@
 package com.example.cse_3311_freshman_app;
 
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-public class ClubProfileActivity {
+public class ClubProfileActivity extends AppCompatActivity {
 
+    ////////////////////////////////////////////////////////////////////////////////
     // Initialize Firebase authentification & database
     FirebaseAuth auth;
     FirebaseFirestore db;
 
     // Initialize activity parts
     // tags
-    TextView location_tag;
-    TextView email_tag;
-    TextView pnumber_tag;
-    TextView address_tag;
-    TextView desc_tag;
+    /*
+    TextView locationTag;
+    TextView emailTag;
+    TextView pnumberTag;
+    TextView addressTag;
+    TextView descTag;
+    */
+
+    // Button
+    Button followButton;
 
     // Firebase Placeholders
-    TextView club_profile_name;
-    TextView location_info;
-    TextView email_info;
-    TextView pnumber_info;
-    TextView address_info;
-    TextView desc_info;
+    TextView clubName;
+    TextView locationInfo;
+    TextView emailInfo;
+    TextView pnumberInfo;
+    TextView addressInfo;
+    TextView descInfo;
+    ImageView clubAvatar;
+
+    ///////////////////////////////////////////////////////////////////////////////
+    // On creation of the page
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        /////////////////////////////////////////
+        // XML File this java file is attached to
+        setContentView(R.layout.activity_club_profile);
+
+        ///////////////////////////////////////
+        // Declare the Buttons and Placeholders
+        followButton = findViewById(R.id.follow_club_button);
+        clubName = findViewById(R.id.club_profile_name);
+        locationInfo = findViewById(R.id.location_info);
+        emailInfo = findViewById(R.id.email_info);
+        pnumberInfo = findViewById(R.id.pnumber_info);
+        addressInfo = findViewById(R.id.address_info);
+        descInfo = findViewById(R.id.description_info);
+        clubAvatar = findViewById(R.id.club_avatar);
 
 
-
+    }
 }
