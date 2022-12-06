f45683d5628854ae0fec560f67f96538526f0d24
Jane Cho <dragoncave48@gmail.com>
2022-12-05 22:13:56 -0600

    DatePicker implement
    DatePicker implemented

--- app/src/main/java/com/example/cse_3311_freshman_app/PostActivity.java
+++ app/src/main/java/com/example/cse_3311_freshman_app/PostActivity.java
@@ -21,6 +21,7 @@ import android.widget.Spinner;
 import android.widget.DatePicker;
 import android.app.DatePickerDialog;
 import android.util.Log;
+import android.app.AlertDialog;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -47,6 +48,8 @@ public class PostActivity extends AppCompatActivity {
     private ImageView captureImage;
     EditText post_des, post_location, post_name, post_org;
     int SELECT_PICTURE = 200;
+    private DatePickerDialog datePickerDialog;
+    private Button dateButton;
 
     FirebaseFirestore db_base;
     CollectionReference db_ref;
@@ -68,6 +71,9 @@ public class PostActivity extends AppCompatActivity {
         post_location = findViewById(R.id.post_location);
         post_name = findViewById(R.id.post_name);
         post_org = findViewById(R.id.post_org);
+        initDatePicker();
+        dateButton = findViewById(R.id.button_date);
+        dateButton.setText(getTodaysDate());
 
         post_backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -154,5 +160,78 @@ public class PostActivity extends AppCompatActivity {
                 }
             }
         }
+    }
+
+    private String getTodaysDate()
+    {
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        month = month + 1;
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        return makeDateString(day, month, year);
+    }
+
+    private void initDatePicker()
+    {
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener()
+        {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day)
+            {
+                month = month + 1;
+                String date = makeDateString(day, month, year);
+                dateButton.setText(date);
+            }
+        };
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        int style = AlertDialog.THEME_HOLO_LIGHT;
+
+        datePickerDialog = new DatePickerDialog(this, style, dateSetListener, year, month, day);
+    }
+
+    private String makeDateString(int day, int month, int year)
+    {
+        return getMonthFormat(month) + " " + day + " " + year;
+    }
+
+    private String getMonthFormat(int month)
+    {
+        if(month == 1)
+            return "JAN";
+        if(month == 2)
+            return "FEB";
+        if(month == 3)
+            return "MAR";
+        if(month == 4)
+            return "APR";
+        if(month == 5)
+            return "MAY";
+        if(month == 6)
+            return "JUN";
+        if(month == 7)
+            return "JUL";
+        if(month == 8)
+            return "AUG";
+        if(month == 9)
+            return "SEP";
+        if(month == 10)
+            return "OCT";
+        if(month == 11)
+            return "NOV";
+        if(month == 12)
+            return "DEC";
+
+        //default should never happen
+        return "JAN";
+    }
+
+    public void openDatePicker(View view)
+    {
+        datePickerDialog.show();
     }
 }
